9f46b9823d20e43368a9ab4ee2cbcc580334a568
Nicolas Moutschen <nicolas.moutschen@gmail.com>
2021-11-23 20:03:36 +0100

    feat: add EnvFilter for tracing_subscriber

--- src/utils.rs
+++ src/utils.rs
@@ -3,7 +3,10 @@ use tracing::{info, instrument};
 
 /// Setup tracing
 pub fn setup_tracing() {
-    let subscriber = tracing_subscriber::fmt().json().finish();
+    let subscriber = tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .json()
+        .finish();
     tracing::subscriber::set_global_default(subscriber).expect("failed to set tracing subscriber");
 }
 
